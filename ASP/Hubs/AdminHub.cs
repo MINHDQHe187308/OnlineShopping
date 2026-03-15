17aaa6fc87fcc3a8452a9200189041b5dd4dff03
DESKTOP-C3NUS3R\Asus <longx3732@gmail.com>
2026-03-15 10:10:13 +0700

    feat(admin) : remove comment

--- ASP/Hubs/AdminHub.cs
+++ ASP/Hubs/AdminHub.cs
@@ -5,7 +5,6 @@ namespace ASP.Hubs
 {
     public class AdminHub : Hub
     {
-        // Hub used for broadcasting Admin-specific events like product variant updates.
-        // Methods can be empty if we only push from Server to Client using IHubContext.
+        
     }
 }
